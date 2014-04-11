2fb05bca7e4d54abc34ad48ffb258ba71bd8894b
Sarah <Sarah@12southmusic.com>
2014-04-11 12:19:53 -0500

    lets try putting it in page-header

--- templates/page-header.php
+++ templates/page-header.php
@@ -3,3 +3,23 @@
     <?php echo roots_title(); ?>
   </h1>
 </div>
+
+
+<!-- Begin Get Started Modal -->  
+<div class="modal fade" id="getStarted" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Get Started Modal in Header</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
