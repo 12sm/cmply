a83c2f385fdb9a2e9c694b1dcb8c470371bd723f
Ian Noyes <ian@12southmusic.com>
2014-04-17 13:07:24 -0500

    text-align center navbar

---
+++ template-landing.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Landing Page Template
+*/
+?>
+
+<?php get_template_part('templates/page', 'header-landing'); ?>
+<?php get_template_part('templates/content', 'page-landing'); ?>
