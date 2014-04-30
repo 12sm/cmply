9cdf7ec77713c9deb558cd57dfb200237b1f3bc7
michael <michael@12southmusic.com>
2014-04-30 17:14:26 -0500

    Update modal dialogs

--- templates/footer-home.php
+++ templates/footer-home.php
@@ -25,13 +25,17 @@
   </div>
 </footer>
 
-<div class="modal fade get-started-modal" id="getStarted" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+<div class="modal fade" id="getStarted" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
       </div>
       <div class="modal-body center">
+      <h2>Want to Learn More?</h2>
+      <h3>Schedule a Demo, Ask a Question, or Activate your Free Trial</h3>
+      <p />
+      <p>Fill out the form below and we'll reach out via email to chat.</p>
       	<?php echo do_shortcode('[formidable id=9]'); ?>
        </div>
       </div>
