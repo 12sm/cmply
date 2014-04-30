e8906c9fb7a67986f1bc177317d5caeefaa87fc1
michael <michael@12southmusic.com>
2014-04-29 22:07:03 -0500

    Push font into WP admin

--- lib/custom.php
+++ lib/custom.php
@@ -2,4 +2,10 @@
 
 define( 'UPLOADS', ''.'assets' );
 
+add_action( 'admin_head' , 'add_open_sans' );
+function add_open_sans() { ?>
+  <script type="text/javascript" src="//use.typekit.net/yeb8ezs.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+
 ?>
