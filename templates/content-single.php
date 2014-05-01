466543aa35ca8f5fcf3faf73848f8f3fa891f50b
Sarah <Sarah@12southmusic.com>
2014-05-01 18:28:06 -0500

    bootstrapping the single post

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,8 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+	 <div class="row">
+	 	<div class="col-sm-12">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php get_template_part('templates/entry-meta'); ?>
+		</div>
+	  </div>
     </header>
     <div class="entry-content">
 	
