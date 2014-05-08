03acb55d6d3d1643ba64b92c3f772d6cdb35bdaa
michael <michael@12southmusic.com>
2014-05-08 12:54:13 -0500

    push head changes

--- templates/footer.php
+++ templates/footer.php
@@ -59,8 +59,7 @@
 </div>
 
 <?php wp_footer(); ?>
-
-<!-- Begin 12SM Network Analytics <!-->   
+ 
   <script type="text/javascript">
 	var _gaq = _gaq || [];
   	_gaq.push(['_setAccount', 'UA-27814560-1']);
@@ -73,4 +72,15 @@
 	  	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  	})();
 	</script>
-  <!-- End 12SM Network Analytics <!--> 
+	
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-11087345-7', 'getcommandpost.com');
+  ga('send', 'pageview');
+
+</script>
+ 
--- templates/head.php
+++ templates/head.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html><!-- 
-   _      ___    ____                
- /' \   /'___`\ /\  _`\   /'\_/`\    
-/\_, \ /\_\ /\ \\ \,\L\_\/\      \   
-\/_/\ \\/_/// /__\/_\__ \\ \ \__\ \  
-   \ \ \  // /_\ \ /\ \L\ \ \ \_/\ \ 
-    \ \_\/\______/ \ `\____\ \_\\ \_\
-     \/_/\/_____/   \/_____/\/_/ \/_/                                                                                                           
- 
+<!DOCTYPE html><![CDATA[
+|   _      ___    ____
+| /' \   /'___`\ /\  _`\   /'\_/`\
+|/\_, \ /\_\ /\ \\ \,\L\_\/\      \
+|\/_/\ \\/_/// /__\/_\__ \\ \ \__\ \
+|   \ \ \  // /_\ \ /\ \L\ \ \ \_/\ \
+|    \ \_\/\______/ \ `\____\ \_\\ \_\
+|     \/_/\/_____/   \/_____/\/_/ \/_/
 built by 12South Music
 http://12southmusic.com/
- 
- -->
+]]> 
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
@@ -32,6 +30,6 @@ http://12southmusic.com/
   <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   
   <script type="text/javascript">var addthis_config = {"data_track_addressbar":false};</script>
-  <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5202adac0a506edf"></script>
+  <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-536131c05a71fb3b"></script>
 
 </head>
