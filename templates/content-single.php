bda0124c953fc0395f3320e91c514a1680f1c04e
michael <michael@12southmusic.com>
2014-05-08 11:51:10 -0500

    Single blog layout change

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,38 +1,38 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(container); ?>>
-    <header>
-	 <div class="row">
-	 	<div class="col-sm-12">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-        <?php get_template_part('templates/entry-meta'); ?>
-		</div>
-	  </div>
-    </header>
-	<div class="row">
-		<div class="col-md-4 featured-image">
-			<div class="entry-content">
-				
-			<?php 
-							
-							if ( '' != get_the_post_thumbnail() ) {
-								the_post_thumbnail( small, $attr );
-							} else {
-								the_title();
-							}
-										
-							?>
-							
-				</div>
-			</div>
-			<div class="col-md-8">
-				<div class="entry-content">
-			 	 <?php the_content(); ?>
-			</div>
-		</div>
-	</div>
+
+<div class="col">
+  <article <?php post_class(); ?>>
+    
+    <div class="row">
+    	<div class="col-md-12 col-sm-12 col-xs-12">
+            <header>
+              <h1 class="entry-title"><?php the_title(); ?></h1>
+              <?php get_template_part('templates/entry-meta'); ?>
+            </header>
+        </div>
+    </div>
+    
+    <div class="row blog-post-content">
+    	<div class="col-md-4 col-sm-4">
+        	<?php echo get_the_post_thumbnail($post_id, 'full', array('class' => 'post-image')); ?>
+        </div>
+    	<div class="col-md-8 col-sm-8">
+    		<div class="entry-content">
+    		  <?php the_content(); ?>
+    		</div>
+         </div>
+     </div>
+   	
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    
+    <div class="row">
+    	<div class="col-md-12 col-sm-12">
+    		<?php comments_template('/templates/comments.php'); ?>
+        </div>
+    </div>
+    
   </article>
+  </div>
 <?php endwhile; ?>
