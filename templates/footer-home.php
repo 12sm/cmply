8f504f164444a45891817c875034f9934e73a542
michael <michael@12southmusic.com>
2014-06-02 15:42:38 -0500

    Remove form from Footer and put in Homepage

--- templates/footer-home.php
+++ templates/footer-home.php
@@ -34,23 +34,6 @@
   </div>
 </footer>
 
-<div class="modal fade" id="getStarted" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-      </div>
-      <div class="modal-body center">
-      <h2>Want to Learn More?</h2>
-      <h3>Schedule a Demo, Ask a Question, or Activate your Free Trial</h3>
-      <p />
-      <p>Fill out the form below and we'll reach out via email to chat.</p>
-      	<?php echo do_shortcode('[formidable id=9]'); ?>
-       </div>
-      </div>
-    </div>
-  </div>
-</div>
 
 <?php wp_footer(); ?>
   
